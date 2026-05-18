8f131d79a398faace77a063c286c242e74317402
hlwqds <hl-ljj@outlook.com>
2026-05-18 11:02:31 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+  BuiltinCmdExit,
+  BuiltinCmdMax,
+} BuiltinCmd;
+
+static const char *builtins[BuiltinCmdMax] = {"exit"};
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -13,6 +20,9 @@ int main(int argc, char *argv[]) {
     fgets(input, sizeof(input), stdin);
     input[sizeof(input) - 1] = '\0';
     input[strlen(input) - 1] = '\0';
+    if (strcmp(input, builtins[BuiltinCmdExit]) == 0) {
+      exit(0);
+    }
     printf("%s: command not found\n", input);
   }
 
